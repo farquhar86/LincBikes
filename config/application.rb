bc9510785b131bfe98772bc489bf5909f992bb54
bradley Farquhar <farquharbradley@gmail.com>
2015-05-02 23:10:27 -0700

    testing

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,13 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.assets.initialize_on_precompile = false
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+
 
 module Linc
   class Application < Rails::Application
@@ -23,6 +27,5 @@ module Linc
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
   end
 end
